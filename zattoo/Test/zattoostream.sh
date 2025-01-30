8ce4ed66bdb824416419da24124a1e95ef24d499
jojo61 <git@jojo61.de>
2025-01-30 22:46:16 +0100

    Missing PATH in zattoostream.sh

--- zattoo/Test/zattoostream.sh
+++ zattoo/Test/zattoostream.sh
@@ -15,6 +15,7 @@ PORT=${2}
 #echo $PARAMETER
 #echo $PORT
 
-PATH="/storage/.kodi"
+KPATH="/storage/.kodi"
+PATH=$PATH":/usr/local/bin"
 
-/usr/local/bin/zattoostream  -p ${PORT} -k ${PATH} -u $PARAMETER 
+/usr/local/bin/zattoostream  -p ${PORT} -k ${KPATH} -u $PARAMETER 
